d875935b1c61be48fbfa195b729ce993483ee687
nanako kan <n.k.0213.0529@gmail.com>
2020-04-20 19:00:33 +0900

    add: 投稿編集機能の実装

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -6,14 +6,29 @@ class PicturesController < ApplicationController
     @picture = Picture.new
   end
   def create
-    Picture.create(picture_params)
-    redirect_to new_picture_path
+    @picture = Picture.create(picture_params)
+    if @picture.save
+      redirect_to pictures_path, notice: "Pictureを作成しました！"
+    else
+      render :new
+    end
   end
   def show
     @picture = Picture.find(params[:id])
   end
-  private
-  def picture_params
-    params.require(:picture).permit(:title, :content)
+  def edit
+    @picture = Picture.find(params[:id])
   end
+  def update
+    @picture = Picture.find(params[:id])
+    if @picture.update(picture_params)
+      redirect_to pictures_path, notice: "Pictureを編集しました！"
+    else
+      render :edit
+    end
+  end
+  private
+    def picture_params
+      params.require(:picture).permit(:title, :content)
+    end
 end
