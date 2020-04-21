c317c11d74ac56068c39da1a08a978c88c67a680
nanako kan <n.k.0213.0529@gmail.com>
2020-04-21 18:10:30 +0900

    add: picture投稿時の確認メール送信機能を実装

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -12,6 +12,7 @@ class PicturesController < ApplicationController
       render :new
     else
       if @picture.save
+        PictureMailer.picture_mail(@picture).deliver
         redirect_to pictures_path, notice: "Pictureを作成しました！"
       else
         render :new
