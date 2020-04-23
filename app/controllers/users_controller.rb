c9746920be800d18afe73d87d419377dcb7c36e4
nanako kan <n.k.0213.0529@gmail.com>
2020-04-23 18:04:59 +0900

    add: プロフィール編集機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,23 @@ class UsersController < ApplicationController
       render :new
     end
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if current_user == @user
+      if @user.update(user_params)
+        flash[:success] = 'ユーザー情報を編集しました。'
+        render :edit
+      else
+        flash.now[:danger] = 'ユーザー情報の編集に失敗しました。'
+        render :edit
+      end
+    else
+      redirect_to root_url
+    end
+  end
   private
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
