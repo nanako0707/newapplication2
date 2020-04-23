91a3669bd83935fa1b5cccd55714341982041ab8
nanako kan <n.k.0213.0529@gmail.com>
2020-04-24 08:03:20 +0900

    add: 投稿者のみ編集できるようアクセス制限をかけ、他人のボタンを非表示に変更

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,6 @@
 class PicturesController < ApplicationController
   before_action :set_picture, only: [:show, :edit, :update, :destroy]
+  before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
   def index
     @pictures = Picture.all
   end
@@ -21,6 +22,7 @@ class PicturesController < ApplicationController
   end
   def show
     @favorite = current_user.favorites.find_by(picture_id: @picture.id)
+    @user = @picture.user
   end
   def edit
   end
@@ -38,6 +40,13 @@ class PicturesController < ApplicationController
   def confirm
     @picture = current_user.pictures.build(picture_params)
     render :new if @picture.invalid?
+  end
+  def ensure_correct_user
+    @picture = Picture.find_by(id: params[:id])
+    if @picture.user_id != current_user.id
+      flash[:notice] = "権限がありません"
+      redirect_to("/pictures/index")
+    end
   end
   private
   def picture_params
