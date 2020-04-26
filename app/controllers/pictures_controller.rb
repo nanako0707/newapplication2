8c6fcc29bd3ffcd6cbffa19ea0f9ddcc5a3aa81c
nanako kan <n.k.0213.0529@gmail.com>
2020-04-26 17:57:44 +0900

    不要なファイル削除

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,52 +2,61 @@ class PicturesController < ApplicationController
   before_action :set_picture, only: [:show, :edit, :update, :destroy]
   before_action :ensure_correct_user, {only: [:edit, :update, :destroy]}
   def index
-    @pictures = Picture.all
+  @pictures = Picture.all
   end
+
   def new
-    @picture = Picture.new
+  @picture = Picture.new
   end
+
   def create
-    @picture = current_user.pictures.build(picture_params)
-    if params[:back]
-      render :new
-    else
-      if @picture.save
-        PictureMailer.picture_mail(@picture).deliver
-        redirect_to pictures_path, notice: "Pictureを作成しました！"
-      else
-        render :new
-      end
-    end
+  @picture = current_user.pictures.build(picture_params)
+  if params[:back]
+  render :new
+  else
+  if @picture.save
+  PictureMailer.picture_mail(@picture).deliver
+  redirect_to pictures_path, notice: "Pictureを作成しました！"
+  else
+  render :new
   end
+  end
+  end
+
   def show
-    @favorite = current_user.favorites.find_by(picture_id: @picture.id)
-    @user = @picture.user
+  @favorite = current_user.favorites.find_by(picture_id: @picture.id)
+  @user = @picture.user
   end
+
   def edit
   end
+
   def update
-    if @picture.update(picture_params)
-      redirect_to pictures_path, notice: "Pictureを編集しました！"
-    else
-      render :edit
-    end
+  if @picture.update(picture_params)
+  redirect_to pictures_path, notice: "Pictureを編集しました！"
+  else
+  render :edit
   end
+  end
+
   def destroy
-    @picture.destroy
-    redirect_to pictures_path, notice: "Pictureを削除しました！"
+  @picture.destroy
+  redirect_to pictures_path, notice: "Pictureを削除しました！"
   end
+
   def confirm
-    @picture = current_user.pictures.build(picture_params)
-    render :new if @picture.invalid?
+  @picture = current_user.pictures.build(picture_params)
+  render :new if @picture.invalid?
   end
+
   def ensure_correct_user
-    @picture = Picture.find_by(id: params[:id])
-    if @picture.user_id != current_user.id
-      flash[:notice] = "権限がありません"
-      redirect_to("/pictures/index")
-    end
+  @picture = Picture.find_by(id: params[:id])
+  if @picture.user_id != current_user.id
+  flash[:notice] = "権限がありません"
+  redirect_to("/pictures/index")
   end
+  end
+  
   private
   def picture_params
   params.require(:picture).permit(:title, :content, :image, :remove_image)
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,39 +1,45 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:id])
+  @user = User.find(params[:id])
   end
+
   def new
-    @user = User.new
+  @user = User.new
   end
+
   def create
-    @user = User.new(user_params)
-    if @user.save
-      redirect_to user_path(@user.id)
-    else
-      render :new
-    end
+  @user = User.new(user_params)
+  if @user.save
+  redirect_to user_path(@user.id)
+  else
+  render :new
   end
+  end
+
   def edit
-    @user = User.find(params[:id])
+  @user = User.find(params[:id])
   end
+
   def update
-    @user = User.find(params[:id])
-    if current_user == @user
-      if @user.update(user_params)
-        flash[:success] = 'ユーザー情報を編集しました。'
-        render :edit
-      else
-        flash.now[:danger] = 'ユーザー情報の編集に失敗しました。'
-        render :edit
-      end
-    else
-      redirect_to "users/new"
-    end
+  @user = User.find(params[:id])
+  if current_user == @user
+  if @user.update(user_params)
+  flash[:success] = 'ユーザー情報を編集しました。'
+  render :edit
+  else
+  flash.now[:danger] = 'ユーザー情報の編集に失敗しました。'
+  render :edit
   end
+  else
+  redirect_to "users/new"
+  end
+  end
+
   def favorites
-    @user = User.find_by(id: params[:id])
-    @favorites = Favorite.where(user_id: @user.id)
+  @user = User.find_by(id: params[:id])
+  @favorites = Favorite.where(user_id: @user.id)
   end
+  
   private
   def user_params
   params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
