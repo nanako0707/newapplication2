dbc48188f233c2c25d2d7104e5685ddb15993f47
nanako kan <n.k.0213.0529@gmail.com>
2020-04-28 18:38:26 +0900

    インデント修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,43 +1,43 @@
 class UsersController < ApplicationController
   def show
-  @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def new
-  @user = User.new
+    @user = User.new
   end
 
   def create
-  @user = User.new(user_params)
-  if @user.save
-  redirect_to new_session_path(@user.id)
-  else
-  render :new
-  end
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_session_path(@user.id)
+    else
+      render :new
+    end
   end
 
   def edit
-  @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def update
-  @user = User.find(params[:id])
-  if current_user == @user
-  if @user.update(user_params)
-  flash[:success] = 'ユーザー情報を編集しました。'
-  render :edit
-  else
-  flash.now[:danger] = 'ユーザー情報の編集に失敗しました。'
-  render :edit
-  end
-  else
-  redirect_to "users/new"
-  end
+    @user = User.find(params[:id])
+    if current_user == @user
+      if @user.update(user_params)
+        flash[:success] = 'ユーザー情報を編集しました。'
+        render :edit
+      else
+        flash.now[:danger] = 'ユーザー情報の編集に失敗しました。'
+        render :edit
+      end
+      else
+        redirect_to "users/new"
+    end
   end
 
   def favorites
-  @user = User.find_by(id: params[:id])
-  @favorites = Favorite.where(user_id: @user.id)
+    @user = User.find_by(id: params[:id])
+    @favorites = Favorite.where(user_id: @user.id)
   end
 
   private
