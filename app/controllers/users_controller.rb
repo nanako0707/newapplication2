036ea4f6e9e47d9002b6a119b76f9fb2dbcb5d8b
nanako kan <n.k.0213.0529@gmail.com>
2020-04-28 18:21:07 +0900

    ログイン後に投稿できるよう修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
   @user = User.new(user_params)
   if @user.save
-  redirect_to user_path(@user.id)
+  redirect_to new_session_path(@user.id)
   else
   render :new
   end
@@ -39,7 +39,7 @@ class UsersController < ApplicationController
   @user = User.find_by(id: params[:id])
   @favorites = Favorite.where(user_id: @user.id)
   end
-  
+
   private
   def user_params
   params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
