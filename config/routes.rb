ab0e35418f21fec123eccee0906aee32fd87060d
nanako kan <n.k.0213.0529@gmail.com>
2020-04-28 18:47:03 +0900

    change: ルート接続時の画面をsign/upに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'sessions#new'
+  root to: 'users#new'
   resources :favorites, only: [:create, :destroy]
   get '/users/:id/favorites', to: 'users#favorites'
   resources :feeds
