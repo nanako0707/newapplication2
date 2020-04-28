d2f9840cd542290febdf237ef48f42de5f11438b
nanako kan <n.k.0213.0529@gmail.com>
2020-04-28 10:24:14 +0900

    投稿画像のサイズ設定

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,6 @@ module Newapplication2
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     I18n.enforce_available_locales = false
-    config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.generators do |g|
